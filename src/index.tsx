0d9eb44a2ddecd030125f49e6040757f2e5226c3
Emre KIRCA <emrekircaa1@gmail.com>
2024-02-12 19:17:58 +0300

    feat : add redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyles from "./lib/GlobalStyle";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 import "styled-components";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,8 +13,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyles />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyles />
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import productSlice from "./product/productSlice";
 import basketSlice from "./basket/cartSlice";
 import brandSlice from "./brands/baskerSlice";
 import tagsSlice from "./tags/tagsSlice";
 import filterSlice from "./filter/filterSlice";
-export const store = configureStore({
-  reducer: {
-    product: productSlice,
-    basket: basketSlice,
-    brand: brandSlice,
-    tag: tagsSlice,
-    filter: filterSlice,
-  },
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "filter",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  product: productSlice,
+  basket: basketSlice,
+  brand: brandSlice,
+  tag: tagsSlice,
+  filter: filterSlice,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
