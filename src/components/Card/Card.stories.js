816dfbf06ff9d5d1b4b03ec616daf770f491d25f
Emre KIRCA <emrekircaa1@gmail.com>
2024-02-14 15:04:41 +0300

    Create Card.stories.js

---
+++ src/components/Card/Card.stories.js
@@ -0,0 +1,23 @@
+import { Card } from ".";
+
+const meta = {
+  title: "Components/Card",
+  component: Card,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const small = {
+  args: {
+    size: "sm",
+  },
+};
+export const large = {
+  args: {
+    size: "lg",
+  },
+};
