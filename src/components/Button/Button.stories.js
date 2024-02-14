8a23f7de5bbcb8ac5f435dc4955a773bdb7b21b6
Emre KIRCA <emrekircaa1@gmail.com>
2024-02-14 15:04:38 +0300

    Create Button.stories.js

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,18 @@
+import { Button } from ".";
+
+const meta = {
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const sample = {
+  args: {
+    children: "Fist story",
+  },
+};
