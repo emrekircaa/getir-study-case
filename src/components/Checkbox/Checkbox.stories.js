2c406a73237b706e1825fddd52151239d4664aac
Emre KIRCA <emrekircaa1@gmail.com>
2024-02-14 15:04:45 +0300

    Create Checkbox.stories.js

---
+++ src/components/Checkbox/Checkbox.stories.js
@@ -0,0 +1,20 @@
+import { Checkbox } from ".";
+
+const meta = {
+  title: "Components/Checkbox",
+  component: Checkbox,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Checked = {
+  args: {
+    label: "Checkbox",
+    isChecked: true,
+    count: 1,
+  },
+};
