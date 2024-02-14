31c5ab143c92d1bf96348acbd5c6694a2f8ee300
Emre KIRCA <emrekircaa1@gmail.com>
2024-02-14 15:04:50 +0300

    Create ShortingRadio.stories.js

---
+++ src/components/ShortingRadio/ShortingRadio.stories.js
@@ -0,0 +1,21 @@
+import { ShortingRadio } from "./index";
+
+const meta = {
+  title: "Components/ShortingRadio",
+  component: ShortingRadio,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const Radio = {
+  args: {
+    selectedOptionId: 1,
+    item: {
+      id: 1,
+    },
+  },
+};
