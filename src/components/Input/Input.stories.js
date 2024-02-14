6fea1dd8db5968b3eaa4e95c99f222b07f607670
Emre KIRCA <emrekircaa1@gmail.com>
2024-02-14 15:04:47 +0300

    Create Input.stories.js

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,19 @@
+import { Input } from "./index";
+
+const meta = {
+  title: "Components/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+export const SampleInput = {
+  args: {
+    value: "value",
+    placeHolder: "placeholder",
+  },
+};
